39f7e1dd589273a5b7bb9ade6cf8fff7408103b1
xiayuanhao <2335961613@qq.com>
2023-02-26 19:08:38 +0800

    项目初始化-调整初始目录结构

--- src/main.js
+++ src/main.js
@@ -3,4 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 引入全局样式文件
+import './styles/index.scss'
+
 createApp(App).use(store).use(router).mount('#app')
